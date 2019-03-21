db0083d5cd6d01fd273b89c67b4f9f31438e8df2
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2019-03-21 13:29:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/newsapi/modelhacker/ResponseHacker.java
@@ -0,0 +1,52 @@
+package com.example.newsapi.modelhacker;
+
+import com.example.newsapi.modelnews.ArticlesItemNews;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ResponseHacker{
+
+	@SerializedName("totalResults")
+	private int totalResults;
+
+	@SerializedName("articles")
+	private List<ArticlesItemNews> articles;
+
+	@SerializedName("status")
+	private String status;
+
+	public void setTotalResults(int totalResults){
+		this.totalResults = totalResults;
+	}
+
+	public int getTotalResults(){
+		return totalResults;
+	}
+
+	public void setArticles(List<ArticlesItemNews> articles){
+		this.articles = articles;
+	}
+
+	public List<ArticlesItemNews> getArticles(){
+		return articles;
+	}
+
+	public void setStatus(String status){
+		this.status = status;
+	}
+
+	public String getStatus(){
+		return status;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"ResponseHacker{" + 
+			"totalResults = '" + totalResults + '\'' + 
+			",articles = '" + articles + '\'' + 
+			",status = '" + status + '\'' + 
+			"}";
+		}
+}
